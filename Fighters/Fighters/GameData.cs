9d64b78474e6961da8b1c8b188d6095d76a9689d
larvalcurd <110689984+larvalcurd@users.noreply.github.com>
2026-05-06 15:08:37 +0300

    использовал [] в GameData.cs

--- Fighters/Fighters/GameData.cs
+++ Fighters/Fighters/GameData.cs
@@ -8,31 +8,31 @@ namespace Fighters;
 public static class GameData
 {
     public static readonly IRace[] Races =
-    {
+    [
         new Human(),
         new Elf(),
         new Orc(),
         new Dwarf()
-    };
+    ];
 
     public static readonly IFighterClass[] FighterClasses =
-    {
+    [
         new Warrior(),
         new Knight(),
         new Rogue()
-    };
+    ];
 
     public static readonly IWeapon[] Weapons =
-    {
+    [
         new Fists(),
         new Axe(),
         new Sword()
-    };
+    ];
 
     public static readonly IArmor[] Armors =
-    {
+    [
         new NoArmor(),
         new LightArmor(),
         new HeavyArmor()
-    };
+    ];
 }
