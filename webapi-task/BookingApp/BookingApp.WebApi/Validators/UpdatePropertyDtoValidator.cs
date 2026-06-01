6b4f7fa070866250685fe31ebdaf9e600a1e1527
kiki <maria.u.r.sun@gmail.com>
2026-06-01 18:04:00 +0300

    забыл добавить

--- webapi-task/BookingApp/BookingApp.WebApi/Validators/UpdatePropertyDtoValidator.cs
+++ webapi-task/BookingApp/BookingApp.WebApi/Validators/UpdatePropertyDtoValidator.cs
@@ -7,12 +7,12 @@ namespace BookingApp.WebApi.Validators
     {
         public UpdatePropertyDtoValidator()
         {
-            RuleFor(x => x.Name).NotEmpty().MaximumLength(200);
-            RuleFor(x => x.Country).NotEmpty().MaximumLength(100);
-            RuleFor(x => x.City).NotEmpty().MaximumLength(100);
-            RuleFor(x => x.Address).NotEmpty().MaximumLength(500);
-            RuleFor(x => x.Latitude).InclusiveBetween(-90m, 90m);
-            RuleFor(x => x.Longitude).InclusiveBetween(-180m, 180m);
+            RuleFor( x => x.Name ).NotEmpty().MaximumLength( 200 );
+            RuleFor( x => x.Country ).NotEmpty().MaximumLength( 100 );
+            RuleFor( x => x.City ).NotEmpty().MaximumLength( 100 );
+            RuleFor( x => x.Address ).NotEmpty().MaximumLength( 500 );
+            RuleFor( x => x.Latitude ).InclusiveBetween( -90m, 90m );
+            RuleFor( x => x.Longitude ).InclusiveBetween( -180m, 180m );
         }
     }
 }
